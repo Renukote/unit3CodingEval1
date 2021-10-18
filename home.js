9d00d17c1a57560424f44cab7d7b6d52ff8bb51d
renukaprasad <renuka_pw1_172@masai.school>
2021-10-18 16:25:36 +0530

    second push

--- home.js
+++ home.js
@@ -4,7 +4,17 @@
 
         function read( data ) {
             window.location.href = 'C:/Users/LENOVO/renu/masai/unit3/codingEval1/news.html';
-            console.log( data );
+            
+
+            let article = JSON.parse(localStorage.getItem('article'));
+
+            if( !article ) {
+                localStorage.setItem('article',JSON.stringify([]))
+            }
+            else {
+                article.push( data );
+                localStorage.setItem('article',JSON.stringify(article));
+            }
         }
     
 
