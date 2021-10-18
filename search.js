ad2272d2b91970f166ceb6f3bc43f63518b051cf
renukaprasad <renuka_pw1_172@masai.school>
2021-10-18 17:09:40 +0530

    implemented search

--- search.js
+++ search.js
@@ -5,12 +5,13 @@ async function showRes(){
     
     let inp = document.getElementById('inp');
     console.log( inp );
-    inp.value = sQuery[0];
+    inp.value = sQuery[sQuery.length-1]
     
     
     let contain = document.getElementById('result');
-
-    let res = await fetch(`http://newsapi.org/v2/everything?q=Apple&from=2021-10-18&sortBy=popularity&apiKey=c277776913874c9293685dec5952cb8e`);
+    
+    console.log( inp.value );
+    let res = await fetch(`http://newsapi.org/v2/everything?q=${inp.value}&from=2021-10-18&sortBy=popularity&apiKey=c277776913874c9293685dec5952cb8e`);
     res = await res.json();
 
     console.log( res );
@@ -19,8 +20,27 @@ async function showRes(){
 
 
 function appendRes( data )
-{
-    let contain = 
+{  
+    data.forEach(( article ) => {
+
+        let container = document.getElementById('result');
+
+        let div = document.createElement('div');
+        
+        let img = document.createElement('img');
+        img.src = article.urlToImage;
+        img.style.width = '200px';
+
+        let headline = document.createElement('h4');
+        headline.innerText = article.title;
+        
+        let desc = document.createElement('div');
+        desc.innerHTML = article.description;
+
+        div.append( img, headline, desc );
+        container.append( div );
+        div.onclick = function () { read( article  ) };
+    })
 }
 
     // console.log( sQuery );
