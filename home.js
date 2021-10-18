8a80e4b46bf8b854f241c8004ec16f28ee9d742d
renukaprasad <renuka_pw1_172@masai.school>
2021-10-18 16:07:31 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,13 @@
+    console.log("this is working");
+
+
+
+        function read( data ) {
+            window.location.href = 'C:/Users/LENOVO/renu/masai/unit3/codingEval1/news.html';
+            console.log( data );
+        }
+    
+
+
+
+
