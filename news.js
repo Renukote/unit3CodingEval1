338691d73aab9a30357d99615374b44d8e317bb6
renukaprasad <renuka_pw1_172@masai.school>
2021-10-18 16:36:48 +0530

    implemented news page

---
+++ news.js
@@ -0,0 +1,32 @@
+
+function appendArticle( data ) {
+    console.log( data )
+
+    data.forEach(( article ) => {
+
+        let container = document.getElementById('article');
+
+        let div = document.createElement('div');
+        
+        let img = document.createElement('img');
+        img.src = article.urlToImage;
+        img.style.width = '200px';
+
+        let headline = document.createElement('h4');
+        headline.innerText = article.title;
+        
+        let desc = document.createElement('div');
+        desc.innerHTML = article.description;
+
+        let link = document.createElement('a');
+        link.href = article.url;  
+        link.innerText = 'click here to view the full article';  
+        console.log( link ) 
+
+        div.append( img, headline, desc, link );
+        container.append( div );
+        // div.onclick = function () { read( article  ) };
+    })
+
+    localStorage.setItem('article',JSON.stringify([]));
+}
